4d15c3c2864f5192e605f36b7e4009517b15f1b7
vikas <vikaspatil212222@gmail.com>
2023-02-26 11:53:57 +0530

    button animation

--- src/Components/main/main.js
+++ src/Components/main/main.js
@@ -46,6 +46,8 @@ const Main = () => {
               EXTRA 10% FOR MEMBERS
             </div>
             <div className=''>
+            <a href='/men' className='a-button'>
+
               <button id='b01' role='button' className='main-btn' onClick={()=>addButtonClass("b01")} >
 
                 SHOP MEN
@@ -54,6 +56,9 @@ const Main = () => {
 
                 </div>
               </button>
+              </a>
+              <a href='/women' className='a-button'>
+
               <button id='b02' role='button' className='main-btn'  onClick={()=>addButtonClass("b02")}>
                 SHOP WOMEN
                 <i class="bi bi-arrow-right fs-4 ms-3"></i>
@@ -61,6 +66,9 @@ const Main = () => {
 
                 </div>
               </button>
+              </a>
+              <a href='/kids' className='a-button'>
+
               <button id='b03' role='button' className='main-btn'  onClick={()=>addButtonClass("b03")}>
                 SHOP KIDS
                 <i class="bi bi-arrow-right fs-4 ms-3"></i>
@@ -68,14 +76,58 @@ const Main = () => {
 
                 </div>
               </button>
+              </a>
             </div>
 
           </div>
         </div>
       </section>
-      <div style={{ height: "1000px" }}>
+      <section className='position-relative'>
+      <picture>
+      <source media="(min-width: 960px)" srcset="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enIN/Images/IN-SS23-UB-LIGHT-ROHIT-IMAGE-D_tcm209-996409.jpg" />
+      <source media="(min-width: 600px)" srcset="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_960,w_960/enIN/Images/IN-SS23-UB-LIGHT-ROHIT-IMAGE-T_tcm209-996412.jpg" />
+      <source srcset="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_600,w_600/enIN/Images/IN-SS23-UB-LIGHT-ROHIT-IMAGE-Mm_tcm209-996410.jpg" />
+      <img width='100%' class="high-res-picture___3SJ41 loaded___3ltPt" id="tcm:209-996413" src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enIN/Images/IN-SS23-UB-LIGHT-ROHIT-IMAGE-D_tcm209-996409.jpg" decoding="async" loading="lazy" />
+      </picture>
+        <div className='button-container-section02 pe-md-5'>
+          <div className='mb-lg-4 pb-lg-4 me-md-5  pe-md-5'>
 
-      </div>
+            <div className='fs-1 fw-bolder d-inline'>
+EPIC ENERGY. LIGHTEST EVER.
+            </div>
+            <div className='fs-5 pb-2'>
+
+            Our lightest Ultraboost ever.
+
+</div>
+            <div className=''>
+            <a href='/member' className='a-button'>
+
+              <button id='b04' role='button' className='main-btn' onClick={()=>addButtonClass("b04")} >
+
+                MEMBERS EARLY ACCESS
+                <i class="bi bi-arrow-right fs-4 ms-3"></i>
+                <div className='border-button'>
+
+                </div>
+              </button>
+              </a>
+              <a href='/explore' className='a-button'>
+
+              <button id='b05' role='button' className='main-btn'  onClick={()=>addButtonClass("b05")}>
+                EXPLORE MORE
+                <i class="bi bi-arrow-right fs-4 ms-3"></i>
+                <div className='border-button'>
+
+                </div>
+              </button>
+              </a>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      
 
 
     </div>
