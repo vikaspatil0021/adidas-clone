dace5294df2fe7890026ac2207dde4ee1d275b3d
vikas <vikaspatil212222@gmail.com>
2023-04-22 12:54:30 +0530

    c

--- src/Components/cart/cart.js
+++ src/Components/cart/cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./cart.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { REMOVE_FROM_CART } from '../../redux/actions/action';
@@ -16,7 +16,13 @@ const Cart = () => {
   })
 
   var x = window.matchMedia("(max-width: 750px)");
-
+// setting initing value of all selects
+  useEffect(()=>{
+    storeData.forEach((each,index)=>{
+      let selectEle= document.querySelector('#select-Qty-' + index);
+      selectEle.value = each.quantity;
+    })
+  },[])
 
   return (
     <div className='d-flex justify-content-center p-3'>
@@ -26,14 +32,15 @@ const Cart = () => {
             YOUR BAG
           </div>
           <div className='mt-3 fs-5'>
-            TOTAL [ {totalQTY} items ] <span className='fw-bold'> {"  ₹" + totalPrice}.00</span>
+            TOTAL [ {totalQTY} items ] <span className='fw-bold'> {"  ₹" + totalPrice}</span>
           </div>
           <div className='mt-2'>
             Items in your bag are not reserved — check out now to make them yours.
 
           </div>
           <div className='mt-4'>
-            {storeData.map((each) => {
+            {storeData.map((each,index) => {
+               
               return (
 
                 <div className='cart-card'>
@@ -41,27 +48,47 @@ const Cart = () => {
                     <img src={each.img1} />
                   </div>
                   <div className='cardProduct-info'>
-                <div className='info-header'>
+                    <div className='info-header'>
 
-                    <div className='fw-bold'>
-                      {each.name}
+                      <div className='fw-bold'>
+                        {each.name}
+                      </div>
+                      <div className='padding-10'>
+                        SIZE : {each.size}
+
+                      </div>
                     </div>
-                    <div className='padding-10'>
-                      SIZE : {each.size}
-                    </div>
-                </div>
                     <div className='info-footer'>
 
                       <div className='padding-10'>
                         ₹ {each.quantity * (parseInt(each.priceTag.replace(' ', '')))}
                       </div>
-                      <div className='cart-qty'>
-                        <span>{(x.matches)?"Qty: ":''}{each.quantity}</span>
-                        <i class='fa-solid fa-angle-down' />
+                      <div>
+
+                        <div className='cart-qty'>
+                          <span>{(x.matches) ? "Qty: " : ''}</span>
+                          <div className='select-div'>
+
+                          <select name="Oty" id={"select-Qty-" + index}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+
+                          </select>
+                          <i  class='fa-solid fa-angle-down cart-Oty-i' />
+                          </div>
+                        </div>
                       </div>
                     </div>
 
-                    <div className='rmItemBtn' onClick={()=>dispatch(REMOVE_FROM_CART(each))}>
+                    <div className='rmItemBtn' onClick={() => dispatch(REMOVE_FROM_CART(each))}>
                       <i class='fa-solid fa-xmark ' />
                     </div>
 
